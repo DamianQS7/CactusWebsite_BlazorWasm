7f4bcb4032cbc292946919893758464a0d5daf40
DamiánQuezada <damianquezada.s@gmail.com>
2024-03-08 09:33:27 -0500

    Add Footer

--- CactusWebsite/wwwroot/scripts/scripts.js
+++ CactusWebsite/wwwroot/scripts/scripts.js
@@ -3,3 +3,23 @@ var jsFunctions = {};
 jsFunctions.getScrollYPosition = function () {
   return window.scrollY;
 };
+
+// const scrollHeader = () => {
+//   const header = document.getElementById("header");
+
+//   this.scrollY >= 50
+//     ? header.classList.add("scroll-header")
+//     : header.classList.remove("scroll-header");
+// };
+
+// window.addEventListener("scroll", scrollHeader);
+
+// const scrollUp = () => {
+//   const scrollUp = document.getElementById("scroll-up");
+
+//   this.scrollY >= 350
+//     ? scrollUp.classList.add("show-scroll")
+//     : scrollUp.classList.remove("show-scroll");
+// };
+
+// window.addEventListener("scroll", scrollUp);
